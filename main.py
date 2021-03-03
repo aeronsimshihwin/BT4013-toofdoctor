401ed500e7f4c3e16d71e091f3ce0ac7b8a64be1
mitchellkwong <mitchellkwong@gmail.com>
2021-03-04 06:10:35 +0800

    Reorganized main.py

--- main.py
+++ main.py
@@ -5,6 +5,96 @@ from models import categorical, numeric
 import strategy
 import utils
 
+def prepare_data(DATE, OPEN, HIGH, LOW, CLOSE, VOL, USA_ADP, USA_EARN,\
+    USA_HRS, USA_BOT, USA_BC, USA_BI, USA_CU, USA_CF, USA_CHJC, USA_CFNAI,\
+    USA_CP, USA_CCR, USA_CPI, USA_CCPI, USA_CINF, USA_DFMI, USA_DUR,\
+    USA_DURET, USA_EXPX, USA_EXVOL, USA_FRET, USA_FBI, USA_GBVL, USA_GPAY,\
+    USA_HI, USA_IMPX, USA_IMVOL, USA_IP, USA_IPMOM, USA_CPIC, USA_CPICM,\
+    USA_JBO, USA_LFPR, USA_LEI, USA_MPAY, USA_MP, USA_NAHB, USA_NLTTF,\
+    USA_NFIB, USA_NFP, USA_NMPMI, USA_NPP, USA_EMPST, USA_PHS, USA_PFED,\
+    USA_PP, USA_PPIC, USA_RSM, USA_RSY, USA_RSEA, USA_RFMI, USA_TVS, USA_UNR,\
+    USA_WINV, exposure, equity, settings):
+    """Returns a dict of dataframes that serves as the standardized data input for all models.
+    
+    Data specification
+    ------------------
+    Each DataFrame in the dict should correspond to a future or an economic indicator.
+    - ? x futures
+    - ? x economic indicators
+    
+    DataFrame specification
+    -----------------------
+    Each column corresponds to some time series associated with the future / economic indicator
+    - Index: datetime
+    - Columns: Price data type: OPEN / CLOSE/ technical indicator name / preprocessed feature name / etc.
+    """
+    date_index = pd.to_datetime(DATE, format='%Y%m%d')
+    data = dict()
+    
+    # Raw X data
+    # data.update({
+    #     'OPEN': pd.DataFrame(OPEN, index=date_index, columns=utils.futuresAllList),
+    #     'HIGH': pd.DataFrame(HIGH, index=date_index, columns=utils.futuresAllList),
+    #     'LOW': pd.DataFrame(LOW, index=date_index, columns=utils.futuresAllList),
+    #     'CLOSE': pd.DataFrame(CLOSE, index=date_index, columns=utils.futuresAllList),
+    #     'VOL': pd.DataFrame(VOL, index=date_index, columns=utils.futuresAllList),
+    # })
+    
+    # Pre-processed X data
+    for future in utils.futuresList:
+        # Slice data by futures
+        df = pd.DataFrame({
+            'OPEN': OPEN[future],
+            'HIGH': HIGH[future],
+            'LOW': LOW[future],
+            'CLOSE': CLOSE[future],
+            'VOL': VOL[future],
+        }, index=date_index)
+        
+        # Technical_indicators
+        # df['technical_indicator_1'] = df['CLOSE'] - df['OPEN']
+        # df['technical_indicator_2'] = np.nan
+        # df['technical_indicator_3'] = np.nan
+
+        # Collate additional information for models
+        # df['categorical_preprocessing_1'] = np.nan
+        # df['arima_preprocessing_1'] = np.nan
+
+        data[future] = df
+    
+    # Economic indicators
+    keys = (
+        'USA_ADP, USA_EARN,\
+        USA_HRS, USA_BOT, USA_BC, USA_BI, USA_CU, USA_CF, USA_CHJC, USA_CFNAI,\
+        USA_CP, USA_CCR, USA_CPI, USA_CCPI, USA_CINF, USA_DFMI, USA_DUR,\
+        USA_DURET, USA_EXPX, USA_EXVOL, USA_FRET, USA_FBI, USA_GBVL, USA_GPAY,\
+        USA_HI, USA_IMPX, USA_IMVOL, USA_IP, USA_IPMOM, USA_CPIC, USA_CPICM,\
+        USA_JBO, USA_LFPR, USA_LEI, USA_MPAY, USA_MP, USA_NAHB, USA_NLTTF,\
+        USA_NFIB, USA_NFP, USA_NMPMI, USA_NPP, USA_EMPST, USA_PHS, USA_PFED,\
+        USA_PP, USA_PPIC, USA_RSM, USA_RSY, USA_RSEA, USA_RFMI, USA_TVS, USA_UNR,\
+        USA_WINV'
+    ).replace(' ', '').split(',')
+    vals = (
+        USA_ADP, USA_EARN,\
+        USA_HRS, USA_BOT, USA_BC, USA_BI, USA_CU, USA_CF, USA_CHJC, USA_CFNAI,\
+        USA_CP, USA_CCR, USA_CPI, USA_CCPI, USA_CINF, USA_DFMI, USA_DUR,\
+        USA_DURET, USA_EXPX, USA_EXVOL, USA_FRET, USA_FBI, USA_GBVL, USA_GPAY,\
+        USA_HI, USA_IMPX, USA_IMVOL, USA_IP, USA_IPMOM, USA_CPIC, USA_CPICM,\
+        USA_JBO, USA_LFPR, USA_LEI, USA_MPAY, USA_MP, USA_NAHB, USA_NLTTF,\
+        USA_NFIB, USA_NFP, USA_NMPMI, USA_NPP, USA_EMPST, USA_PHS, USA_PFED,\
+        USA_PP, USA_PPIC, USA_RSM, USA_RSY, USA_RSEA, USA_RFMI, USA_TVS, USA_UNR,\
+        USA_WINV,
+    )
+    for key, val in zip(keys, vals):
+        data[key] = pd.DataFrame(
+            data = val,
+            index = date_index,
+            columns = ['CLOSE'],
+        )
+    
+    return data
+    
+
 def myTradingSystem(DATE, OPEN, HIGH, LOW, CLOSE, VOL, USA_ADP, USA_EARN,\
     USA_HRS, USA_BOT, USA_BC, USA_BI, USA_CU, USA_CF, USA_CHJC, USA_CFNAI,\
     USA_CP, USA_CCR, USA_CPI, USA_CCPI, USA_CINF, USA_DFMI, USA_DUR,\
@@ -15,95 +105,84 @@ def myTradingSystem(DATE, OPEN, HIGH, LOW, CLOSE, VOL, USA_ADP, USA_EARN,\
     USA_PP, USA_PPIC, USA_RSM, USA_RSY, USA_RSEA, USA_RFMI, USA_TVS, USA_UNR,\
     USA_WINV, exposure, equity, settings):
     ''' This system uses trend following techniques to allocate capital into the desired equities'''
-    # Wrap wrap in pandas because we are using pd.Series as input
+
+    # Prepare standardized model input
     date_index = pd.to_datetime(DATE, format='%Y%m%d')
-    OPEN = pd.DataFrame(OPEN, index=date_index, columns=utils.futuresAllList)
-    HIGH = pd.DataFrame(HIGH, index=date_index, columns=utils.futuresAllList)
-    LOW = pd.DataFrame(LOW, index=date_index, columns=utils.futuresAllList)
-    CLOSE = pd.DataFrame(CLOSE, index=date_index, columns=utils.futuresAllList)
-    VOL = pd.DataFrame(VOL, index=date_index, columns=utils.futuresAllList)
-    
-    # Collate additional information for models
-    # technical_indicators = utils.technical_indicators(OPEN, HIGH, LOW, CLOSE, VOL)
-    technical_indicators = None
-    economic_indicators = pd.DataFrame(
-        data = np.hstack([
-            USA_ADP, USA_EARN,\
-            USA_HRS, USA_BOT, USA_BC, USA_BI, USA_CU, USA_CF, USA_CHJC, USA_CFNAI,\
-            USA_CP, USA_CCR, USA_CPI, USA_CCPI, USA_CINF, USA_DFMI, USA_DUR,\
-            USA_DURET, USA_EXPX, USA_EXVOL, USA_FRET, USA_FBI, USA_GBVL, USA_GPAY,\
-            USA_HI, USA_IMPX, USA_IMVOL, USA_IP, USA_IPMOM, USA_CPIC, USA_CPICM,\
-            USA_JBO, USA_LFPR, USA_LEI, USA_MPAY, USA_MP, USA_NAHB, USA_NLTTF,\
-            USA_NFIB, USA_NFP, USA_NMPMI, USA_NPP, USA_EMPST, USA_PHS, USA_PFED,\
-            USA_PP, USA_PPIC, USA_RSM, USA_RSY, USA_RSEA, USA_RFMI, USA_TVS, USA_UNR,\
-            USA_WINV,
-        ]),
-        columns = (
-            'USA_ADP, USA_EARN,\
-            USA_HRS, USA_BOT, USA_BC, USA_BI, USA_CU, USA_CF, USA_CHJC, USA_CFNAI,\
-            USA_CP, USA_CCR, USA_CPI, USA_CCPI, USA_CINF, USA_DFMI, USA_DUR,\
-            USA_DURET, USA_EXPX, USA_EXVOL, USA_FRET, USA_FBI, USA_GBVL, USA_GPAY,\
-            USA_HI, USA_IMPX, USA_IMVOL, USA_IP, USA_IPMOM, USA_CPIC, USA_CPICM,\
-            USA_JBO, USA_LFPR, USA_LEI, USA_MPAY, USA_MP, USA_NAHB, USA_NLTTF,\
-            USA_NFIB, USA_NFP, USA_NMPMI, USA_NPP, USA_EMPST, USA_PHS, USA_PFED,\
-            USA_PP, USA_PPIC, USA_RSM, USA_RSY, USA_RSEA, USA_RFMI, USA_TVS, USA_UNR,\
-            USA_WINV'
-        ).replace(' ', '').split(','),
-        index = date_index,
+    data = dict()
+
+    for future in utils.futuresList:
+        # Slice data by futures
+        df = pd.DataFrame({
+            'OPEN': OPEN[future],
+            'HIGH': HIGH[future],
+            'LOW': LOW[future],
+            'CLOSE': CLOSE[future],
+            'VOL': VOL[future],
+        }, index=date_index)
+        pass # Technical_indicators
+        pass # Preprocessed features
+        data[future] = df
+
+    keys = (
+        'USA_ADP, USA_EARN,\
+        USA_HRS, USA_BOT, USA_BC, USA_BI, USA_CU, USA_CF, USA_CHJC, USA_CFNAI,\
+        USA_CP, USA_CCR, USA_CPI, USA_CCPI, USA_CINF, USA_DFMI, USA_DUR,\
+        USA_DURET, USA_EXPX, USA_EXVOL, USA_FRET, USA_FBI, USA_GBVL, USA_GPAY,\
+        USA_HI, USA_IMPX, USA_IMVOL, USA_IP, USA_IPMOM, USA_CPIC, USA_CPICM,\
+        USA_JBO, USA_LFPR, USA_LEI, USA_MPAY, USA_MP, USA_NAHB, USA_NLTTF,\
+        USA_NFIB, USA_NFP, USA_NMPMI, USA_NPP, USA_EMPST, USA_PHS, USA_PFED,\
+        USA_PP, USA_PPIC, USA_RSM, USA_RSY, USA_RSEA, USA_RFMI, USA_TVS, USA_UNR,\
+        USA_WINV'
+    ).replace(' ', '').split(',')
+    vals = (
+        USA_ADP, USA_EARN,\
+        USA_HRS, USA_BOT, USA_BC, USA_BI, USA_CU, USA_CF, USA_CHJC, USA_CFNAI,\
+        USA_CP, USA_CCR, USA_CPI, USA_CCPI, USA_CINF, USA_DFMI, USA_DUR,\
+        USA_DURET, USA_EXPX, USA_EXVOL, USA_FRET, USA_FBI, USA_GBVL, USA_GPAY,\
+        USA_HI, USA_IMPX, USA_IMVOL, USA_IP, USA_IPMOM, USA_CPIC, USA_CPICM,\
+        USA_JBO, USA_LFPR, USA_LEI, USA_MPAY, USA_MP, USA_NAHB, USA_NLTTF,\
+        USA_NFIB, USA_NFP, USA_NMPMI, USA_NPP, USA_EMPST, USA_PHS, USA_PFED,\
+        USA_PP, USA_PPIC, USA_RSM, USA_RSY, USA_RSEA, USA_RFMI, USA_TVS, USA_UNR,\
+        USA_WINV,
     )
+    for key, val in zip(keys, vals):
+        data[key] = pd.DataFrame(
+            data = val,
+            index = date_index,
+            columns = ['CLOSE'],
+        )
     
-    # Generate predictions, sign and magnitude for each model
+    # Load saved models
+    models = dict()
+    for (name, Model, args, kwargs, save_file) in categorical.models + numeric.models:
+        models[name] = Model(*args, **kwargs)
+        models[name].load(save_file)
+
+    # Fit and predict
     prediction = pd.DataFrame(index=utils.futuresList)
     sign = pd.DataFrame(index=utils.futuresList)
     magnitude = pd.DataFrame(index=utils.futuresList)
+    
+    for name, model in models.items():
+        # Models choose which predictors to use in implementation
+        # Predictions in pd.Series with index = utils.futuresList
+        preds = model.fit_predict(data)
 
-    for model_name, model_wrapper in categorical.models.items():
-        probs = model_wrapper(
-            OPEN, HIGH, LOW, CLOSE, VOL,
-            technical_indicators = technical_indicators,
-            economic_indicators = economic_indicators, 
-        )
-        prediction[model_name] = probs
-        sign[model_name] = utils.sign(probs)
-        magnitude[model_name] = utils.magnitude(probs)
+        prediction[name] = preds
+        sign[name] = utils.sign(preds)
+        magnitude[name] = utils.magnitude(preds)
 
-    for model_name, model_wrapper in numeric.models.items():
-        price_diff = model_wrapper(
-            OPEN, HIGH, LOW, CLOSE, VOL,
-            technical_indicators = technical_indicators,
-            economic_indicators = economic_indicators, 
-        )
-        prediction[model_name] = price_diff
-        sign[model_name] = utils.sign(price_diff)
-        magnitude[model_name] = utils.magnitude(price_diff)
+    # Allow strategy stage to access all prediction in addition to raw data
+    data['prediction'] = prediction
+    data['sign'] = sign
+    data['magnitude'] = magnitude
 
-    # Determine trade positions for a set of strategies
-    # positions = pd.DataFrame(index=utils.futuresList)
-
-    # for model_name, model_wrapper in strategy.models.items():
-    #     positions[model_name] = model_wrapper(
-    #         OPEN, HIGH, LOW, CLOSE, VOL,
-    #         technical_indicators = technical_indicators,
-    #         economic_indicators = economic_indicators, 
-    #         prediction = prediction,
-    #         sign = sign,
-    #         magnitude = magnitude,
-    #     )
-
-    positions = prediction # Stub
-
-    # Consolidate positions from all strategies into a final position?
-    # - Actually I think we are just hardcoding in the best strategy based on
-    #   sharpe ratio right? Maybe someone can change this part haha
-    aggregator = lambda x: x.mean(axis=1) # Stub
-    position = aggregator(positions)
+    # Futures strategy
+    position = data['prediction'].sample(axis='columns') # Stub: random sample
 
     # Cash-futures strategy
     cash_frac = 0.0
-    position = np.array([cash_frac, *position]) # Stub
-
-    # Normalization bc it doesn't hurt I guess
-    weights = position/np.nansum(np.abs(position))
+    weights = np.array([cash_frac, *position]) # Stub: ignore cash
 
     # Yay!
     print('Another day passes...')
