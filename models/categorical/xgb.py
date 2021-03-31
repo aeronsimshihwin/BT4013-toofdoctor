d0d641adbf61d7feb6064eafd8b5a4ae05d86365
laiyanjean <laiyanjean@gmail.com>
2021-03-31 10:55:46 +0800

    Add threshold parameter in predict function

--- models/categorical/xgb.py
+++ models/categorical/xgb.py
@@ -23,15 +23,16 @@ class XGBWrapper:
         if future in data:
             self._fit(*self._y_X(data, future), **kwargs)
 
-    def predict(self, data, future, **kwargs):
+    def predict(self, data, future, threshold=0.5, **kwargs):
         try:
             y, X = self._y_X(data, future)
             y_pred = self.model.predict_proba(X)
             y_pred_pos = y_pred[:, 1][-1] # get probability of class 1
             y_pred_norm = y_pred_pos - 0.5 # normalise to include long and short
-            y_pred_norm_long = max(0,y_pred_norm) # long only
+            y_pred_norm_long = max(0, y_pred_pos - threshold) # long only
             return y_pred_norm_long # returns only last value
         except:
+            print("invalid")
             return 0 # input invalid
 
     def _y_X(self, data, future):
